3fc0c850321a15ca607c9d545cc0ed19caf8278b
Alan Chen <alrichen@ucsc.edu>
2025-03-03 01:00:32 -0800

    Tried both excercises

--- lab06/js/script.js
+++ lab06/js/script.js
@@ -3,11 +3,22 @@ let futureValue;
 let investment;
 let rate;
 let years;
+const MAX_RATE = 6;
+const MAX_YEARS = 30;
 
 // COLLECT VALUES FROM THE USER
-investment = parseFloat(prompt('Enter investment amount as xxxx.xx'));
-rate = parseFloat(prompt('Enter interest rate as xx.x'));
-years = parseInt(prompt('Enter the number of years you want to invest for'));
+do {
+    investment = parseFloat(prompt('Enter investment amount as xxxx.xx\nEnter a number greater than 0.'));
+} while (isNaN(investment) || investment < 0)
+console.log(investment);
+do {
+    rate = parseFloat(prompt('Enter interest rate as xx.x\nEnter a realistic number between 0 and 6.'));
+} while (isNaN(rate) || rate < 0 || rate > MAX_RATE)
+console.log(rate);
+do {
+    years = parseInt(prompt('Enter the number of years you want to invest for.\nEnter a number between 1 and 30.'));
+} while (isNaN(years) || years < 1 || years > MAX_YEARS)
+console.log(years);
 
 // CALCULATE FUTURE VALUE
 futureValue = investment;
